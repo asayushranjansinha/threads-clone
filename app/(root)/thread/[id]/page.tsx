2969caeab0c28d3aa70d886c2a037f248a2a53f1
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2023-08-09 15:16:11 +0530

    feat: thread post and replies added

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,24 +1,49 @@
-import ThreadCard from "@/components/cards/ThreadCard";
-import { fetchThreadById } from "@/lib/actions/thread.action";
-import { fetchUser } from "@/lib/actions/user.actions";
-import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
 
-const Page = async ({ params }: { params: { id: string } }) => {
+import Comment from "@/components/forms/Comment";
+import ThreadCard from "@/components/cards/ThreadCard";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchThreadById } from "@/lib/actions/thread.action";
+
+export const revalidate = 0;
+
+/**
+ * Renders a page with a thread and comments.
+ *
+ * @param {Object} params - The parameters for the page.
+ * @param {string} params.id - The ID of the thread.
+ *
+ * @returns {JSX.Element|null} - The page component.
+ */
+async function page({
+  params,
+}: {
+  params: { id: string };
+}): Promise<JSX.Element | null> {
+  // Check if the ID is provided
   if (!params.id) return null;
+
+  // Fetch the current user
   const user = await currentUser();
+  if (!user) return null;
+
+  // Fetch user info
   const userInfo = await fetchUser(user.id);
-  if (userInfo.onboarded === false) redirect("/onboarding");
+  // Redirect to onboarding if the user is not onboarded
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
+  // Fetch the thread by ID
+  const thread = await fetchThreadById(params.id);
 
-    const thread = await fetchThreadById(params.id);
-
+  // Render the thread card and comments
   return (
     <section className="relative">
       <div>
         <ThreadCard
           id={thread._id}
-          currentUserId={user?.id || ""}
+          currentUserId={user.id}
           parentId={thread.parentId}
           content={thread.text}
           author={thread.author}
@@ -27,7 +52,31 @@ const Page = async ({ params }: { params: { id: string } }) => {
           comments={thread.children}
         />
       </div>
+      <div className="mt-7">
+        <Comment
+          threadId={params.id}
+          currentUserImg={user.imageUrl}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user.id}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
+      </div>
     </section>
   );
-};
-export default Page;
+}
+
+export default page;
